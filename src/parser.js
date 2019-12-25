d9b902e683e7971dfdfcb9c91142f3186bb7c7bd
Eldar Babaev <el.glagius@gmail.com>
2019-12-25 17:08:38 +0700

    Changed parser logic

--- src/parser.js
+++ src/parser.js
@@ -1,16 +1,42 @@
-const compare = (oldObj = {}, newObj = {}) => {
-  const hasProperty = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
-  const unitedObject = { ...oldObj, ...newObj };
-  const keys = Object.keys(unitedObject);
+import { getKeys, hasProperty } from './utils';
+
+const getComparisonResult = (oldObj = {}, newObj = {}) => {
+  const mergedObject = { ...oldObj, ...newObj };
+  const keys = getKeys(mergedObject);
   const compareValues = (obj1, obj2, key) => {
-    if (!obj1 || !hasProperty(obj1, key)) return [{ value: obj2[key], status: 'added' }];
-    if (!obj2 || !hasProperty(obj2, key)) return [{ value: obj1[key], status: 'removed' }];
-    if (obj1[key] === obj2[key]) return obj1[key];
-    return [{ status: 'removed', value: obj1[key] }, { status: 'added', value: obj2[key] }];
+    if (!obj1 || !hasProperty(obj1, key)) return { status: 'added', changes: [{ value: obj2[key], status: 'added' }] };
+    if (!obj2 || !hasProperty(obj2, key)) return { status: 'removed', changes: [{ value: obj1[key], status: 'removed' }] };
+    if (obj1[key] === obj2[key]) return { status: 'unchanged', changes: null };
+    if (typeof obj1[key] === typeof obj2[key] && typeof obj1[key] === 'object') return { status: 'unchanged', changes: null };
+    return { status: 'changed', changes: [{ value: obj1[key], status: 'removed' }, { value: obj2[key], status: 'added' }] };
   };
-  return keys.reduce((acc, key) => (typeof unitedObject[key] === 'object'
-    ? { ...acc, [key]: compare(oldObj[key], newObj[key]) }
-    : { ...acc, [key]: compareValues(oldObj, newObj, key) }), {});
+  return keys.reduce((acc, key) => {
+    if (typeof mergedObject[key] === 'object') {
+      return {
+        ...acc,
+        children: [
+          ...acc.children,
+          {
+            key,
+            value: mergedObject[key],
+            ...compareValues(oldObj, newObj, key),
+            ...getComparisonResult(oldObj[key], newObj[key]),
+          },
+        ],
+      };
+    }
+    return {
+      ...acc,
+      children: [
+        ...acc.children,
+        {
+          key,
+          value: mergedObject[key],
+          ...compareValues(oldObj, newObj, key),
+        },
+      ],
+    };
+  }, { children: [] });
 };
 
-export default compare;
+export default getComparisonResult;
