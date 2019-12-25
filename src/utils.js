57db0de967a7fa19897625a4d2f3c7c2f82ff838
Eldar Babaev <el.glagius@gmail.com>
2019-12-25 17:09:05 +0700

    Added some helpers

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,7 @@ const getCurrentPath = filepath => (
   path.isAbsolute(filepath)
     ? filepath
     : path.normalize(filepath));
+
 const getFileType = (filepath) => {
   const type = path.extname(filepath) ? path.extname(filepath).slice(1) : false;
   const classesByType = {
@@ -16,16 +17,12 @@ const getFileType = (filepath) => {
   };
   return type ? classesByType[type]() : false;
 };
-// TODO: move it to "compareData";
-// const isEqualNodes = (node1, node2, property) => {
-//   const props = {
-//     type: () => node1.type === node2.type,
-//     path: () => parsePath(node1.key) === parsePath(node2.key),
-//     value: () => node1.type === node2.type && node1.type === 'simple' && node1.value === node2.value,
-//   };
-//   return props[property]();
-// };
 
+const getKeys = obj => Object.keys(obj);
+const cleanArray = arr => arr.filter(Boolean);
+const hasProperty = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
 const readFile = filepath => getFileType(getCurrentPath(filepath));
 
-export { readFile };
+export {
+  readFile, getKeys, cleanArray, hasProperty,
+};
