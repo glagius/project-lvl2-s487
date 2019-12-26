f8d244ae69f05618e4aff4ddaa282c8b23b4bcfb
Eldar Babaev <el.glagius@gmail.com>
2019-12-26 15:00:38 +0700

    Changed linter config

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,11 +11,11 @@ describe('Test renders output', () => {
   ${'yml'}  | ${'plain'}
   ${'ini'}  | ${'nested'}
   ${'ini'}  | ${'plain'}`('Render $renderFormat type of $fileType diff', ({ fileType, renderFormat }) => {
-  const oldFile = `./__tests__/__fixtures__/${fileType}/oldConfig.${fileType}`;
-  const newFile = `./__tests__/__fixtures__/${fileType}/newConfig.${fileType}`;
-  const comparedDiff = genDiff(oldFile, newFile, renderFormat);
-  expect(comparedDiff).toBe(getText(`./__tests__/__fixtures__/results/${renderFormat}Result.txt`));
-});
+    const oldFile = `./__tests__/__fixtures__/${fileType}/oldConfig.${fileType}`;
+    const newFile = `./__tests__/__fixtures__/${fileType}/newConfig.${fileType}`;
+    const comparedDiff = genDiff(oldFile, newFile, renderFormat);
+    expect(comparedDiff).toBe(getText(`./__tests__/__fixtures__/results/${renderFormat}Result.txt`));
+  });
 });
 describe('Test engine workflow', () => {
   test('Get AST in JSON', async () => {
