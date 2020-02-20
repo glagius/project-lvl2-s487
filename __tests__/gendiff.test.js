e1b26d86f9d4be5313f26232e277845c6e98b86f
Eldar Babaev <el.glagius@gmail.com>
2020-02-20 17:57:00 +0700

    Refactored tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,28 +1,31 @@
-/* eslint-disable indent */
 import fs from 'fs';
 import genDiff from '..';
 
-const getText = filepath => fs.readFileSync(filepath, 'utf8');
+const getFilePath = (fileType, contentType) => fileType === 'txt' ?
+  `./__tests__/__fixtures__/results/${contentType}.${fileType}`
+  :
+  `./__tests__/__fixtures__/${contentType}/config.${fileType}`;
+const getText = (filepath) => fs.readFileSync(filepath, 'utf8');
+
 describe('Test renders output', () => {
-  test.each`
-  fileType  | renderFormat
+  test.each`fileType  | renderFormat
   ${'json'} | ${'nested'}
   ${'json'} | ${'plain'}
   ${'yml'}  | ${'nested'}
   ${'yml'}  | ${'plain'}
   ${'ini'}  | ${'nested'}
   ${'ini'}  | ${'plain'}`('Render $renderFormat type of $fileType diff', ({ fileType, renderFormat }) => {
-    const oldContent = `./__tests__/__fixtures__/${fileType}/oldConfig.${fileType}`;
-    const newContent = `./__tests__/__fixtures__/${fileType}/newConfig.${fileType}`;
+    const oldContent = getFilePath(fileType, 'oldConfig');
+    const newContent = getFilePath(fileType, 'newConfig');
     const comparedDiff = genDiff(oldContent, newContent, renderFormat);
-    expect(comparedDiff).toBe(getText(`./__tests__/__fixtures__/results/${renderFormat}Result.txt`));
+    expect(comparedDiff).toBe(getText(getFilePath('txt', renderFormat)));
   });
 });
 describe('Test engine workflow', () => {
   test('Get AST in JSON', async () => {
-    const oldContent = './__tests__/__fixtures__/json/oldConfig.json';
-    const newContent = './__tests__/__fixtures__/json/newConfig.json';
-    const stringifiedJSON = getText('./__tests__/__fixtures__/results/jsonResult.txt');
+    const oldContent = getFilePath('json', 'oldConfig');
+    const newContent = getFilePath('json', 'newConfig');;
+    const stringifiedJSON = getText(getFilePath('txt', 'json'));
     const comparedConfig = genDiff(oldContent, newContent, 'json');
     expect(JSON.stringify(comparedConfig)).toBe(stringifiedJSON);
   });
