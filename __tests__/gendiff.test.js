0fd8ba1b1e6989d56601a4384657fcc53e8e6918
Eldar Babaev <el.glagius@gmail.com>
2020-02-21 22:35:45 +0700

    Refactored tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,20 +1,21 @@
 import fs from 'fs';
 import genDiff from '..';
 
-const getFilePath = (fileType, contentType) => fileType === 'txt' ?
-  `./__tests__/__fixtures__/results/${contentType}.${fileType}`
-  :
-  `./__tests__/__fixtures__/${contentType}/config.${fileType}`;
+const getFilePath = (fileType, contentType) => {
+  if (fileType === 'txt') return `./__tests__/__fixtures__/results/${contentType}.${fileType}`;
+  return `./__tests__/__fixtures__/${contentType}/config.${fileType}`;
+};
 const getText = (filepath) => fs.readFileSync(filepath, 'utf8');
 
 describe('Test renders output', () => {
-  test.each`fileType  | renderFormat
-  ${'json'} | ${'nested'}
-  ${'json'} | ${'plain'}
-  ${'yml'}  | ${'nested'}
-  ${'yml'}  | ${'plain'}
-  ${'ini'}  | ${'nested'}
-  ${'ini'}  | ${'plain'}`('Render $renderFormat type of $fileType diff', ({ fileType, renderFormat }) => {
+  test.each([
+    ['nested', 'json'],
+    ['plain', 'json'],
+    ['nested', 'yml'],
+    ['plain', 'yml'],
+    ['nested', 'ini'],
+    ['plain', 'ini'],
+  ])('Render %s diff of %s files', (renderFormat, fileType) => {
     const oldContent = getFilePath(fileType, 'oldConfig');
     const newContent = getFilePath(fileType, 'newConfig');
     const comparedDiff = genDiff(oldContent, newContent, renderFormat);
@@ -24,7 +25,7 @@ describe('Test renders output', () => {
 describe('Test engine workflow', () => {
   test('Get AST in JSON', async () => {
     const oldContent = getFilePath('json', 'oldConfig');
-    const newContent = getFilePath('json', 'newConfig');;
+    const newContent = getFilePath('json', 'newConfig');
     const stringifiedJSON = getText(getFilePath('txt', 'json'));
     const comparedConfig = genDiff(oldContent, newContent, 'json');
     expect(JSON.stringify(comparedConfig)).toBe(stringifiedJSON);
