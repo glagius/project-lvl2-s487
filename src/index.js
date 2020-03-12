b11198fc04dba4b892068d5e5c4fd4e129bc5842
Eldar Babaev <el.glagius@gmail.com>
2020-03-13 01:46:44 +0700

    Minor changes to logic and lodash imports

--- src/index.js
+++ src/index.js
@@ -1,49 +1,45 @@
+import has from 'lodash/has';
+import union from 'lodash/union';
+import keys from 'lodash/keys';
+import isObject from 'lodash/isObject';
 import path from 'path';
-import * as _ from 'lodash';
-import { readINI, readJSON, readYAML } from './parsers';
+
+import read from './reader';
 import render from './formatters';
 
-const getCurrentPath = (filepath) => path.resolve(filepath);
-
-const getContent = (contentPath) => {
-  const filepath = getCurrentPath(contentPath);
-  const type = path.extname(filepath) ? path.extname(filepath).slice(1) : false;
-  const conventers = {
-    json: readJSON,
-    yml: readYAML,
-    ini: readINI,
-  };
-  return type ? conventers[type](filepath) : new Error('Wrong "filepath"');
+const parseFilePath = (filepath) => {
+  const absolutePath = path.resolve(filepath);
+  const filetype = path.extname(filepath) ? path.extname(filepath).slice(1) : null;
+  return { absolutePath, filetype };
 };
 
 const compareValues = (obj1, obj2, key) => {
-  if (!_.has(obj1, key)) return { status: 'added', newValue: obj2[key] };
-  if (!_.has(obj2, key)) return { status: 'removed', oldValue: obj1[key] };
+  if (!has(obj1, key)) return { status: 'added', newValue: obj2[key] };
+  if (!has(obj2, key)) return { status: 'removed', oldValue: obj1[key] };
   if (obj1[key] === obj2[key] || (
-    _.isObject(obj1[key]) && _.isObject(obj2[key])
+    isObject(obj1[key]) && isObject(obj2[key])
   )) return { status: 'unchanged', oldValue: obj1[key] };
   return { status: 'changed', oldValue: obj1[key], newValue: obj2[key] };
 };
 
 const compare = (oldObject, newObject) => {
-  const keys = _.union(_.keys(oldObject), _.keys(newObject));
-  return keys.map((key) => {
+  const mergedKeys = union(keys(oldObject), keys(newObject));
+  return mergedKeys.map((key) => {
     const changes = compareValues(oldObject, newObject, key);
-    if (typeof newObject[key] === 'object') {
+    if (isObject(newObject[key]) && isObject(oldObject[key])) {
       const children = compare(oldObject[key], newObject[key]);
-      return {
-        key,
-        ...changes,
-        children: children.map((child) => ({ ...child, parent: key })),
-      };
+      return { key, children, ...changes };
     }
     return { key, ...changes };
   });
 };
 
-export default (path1, path2, format = 'nested') => {
-  const oldConfig = getContent(path1);
-  const newConfig = getContent(path2);
-  const ast = compare(oldConfig, newConfig);
-  return render(format, ast);
+export default (filepath1, filepath2, format = 'nested') => {
+  const { absolutePath: absolutePath1, filetype: filetype1 } = parseFilePath(filepath1);
+  const { absolutePath: absolutePath2, filetype: filetype2 } = parseFilePath(filepath2);
+  const config1 = read(absolutePath1, filetype1);
+  const config2 = read(absolutePath2, filetype2);
+  const ast = compare(config1, config2);
+
+  return render(ast, format);
 };
