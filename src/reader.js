9c0c944e5c88f461c333049ac454c1ccb3d76f7e
Eldar Babaev <el.glagius@gmail.com>
2020-03-13 01:44:28 +0700

    Changed reader logic

--- src/reader.js
+++ src/reader.js
@@ -1,9 +1,18 @@
 import ini from 'ini';
 import fs from 'fs';
 import yaml from 'js-yaml';
+import has from 'lodash/has';
 
 const readJSON = (filepath) => JSON.parse(fs.readFileSync(filepath));
 const readYAML = (filepath) => yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
 const readINI = (filepath) => ini.parse(fs.readFileSync(filepath, 'utf8'));
 
-export { readINI, readJSON, readYAML };
+export default (filepath, filetype) => {
+  const converters = {
+    json: readJSON,
+    yml: readYAML,
+    ini: readINI,
+  };
+
+  return has(converters, filetype) ? converters[filetype](filepath) : new Error('Wrong "filepath"');
+};
